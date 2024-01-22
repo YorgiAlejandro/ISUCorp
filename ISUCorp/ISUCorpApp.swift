1e2979976e0c7ae7a9c1706edae3f30796124e4b
YorgiAlejandro <yorgidelriomarquez@gmail.com>
2024-01-22 17:28:11 -0500

    Initial Commit

---
+++ ISUCorp/ISUCorpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISUCorpApp.swift
+//  ISUCorp
+//
+//  Created by BeMillionaire on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ISUCorpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
